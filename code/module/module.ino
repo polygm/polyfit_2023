4b9bd73e15aa29ed76f3e6df76c3040e98d6265a
HojinLee <infohojin@gmail.com>
2023-08-07 16:37:12 +0900

    인터럽트2개 정역제여

--- code/module/module.ino
+++ code/module/module.ino
@@ -21,7 +21,8 @@ int CW[4] = { 0b1000, 0b0100, 0b0010,0b0001 };
 int CCW[4] = { 0b0001, 0b0010, 0b0100, 0b1000 };
 
 // 모터 활성화: 1=동작, 0=정지
-int mortor_enable = 1;
+int mortor_enable1 = 1;
+int mortor_enable2 = 1;
 int angle; // 모터가 돌아가야 되는 계산돤 펄스
 
 // 초기화
@@ -41,7 +42,12 @@ void setup(){
   // 포토센서를 위한 인터럽트
   attachInterrupt( 
     digitalPinToInterrupt(2), 
-    buttonPress, 
+    buttonPress1, 
+    FALLING);
+
+  attachInterrupt( 
+    digitalPinToInterrupt(3), 
+    buttonPress2, 
     FALLING);
 
   Serial.begin(115200);
@@ -60,17 +66,31 @@ void stepDelay()
 
 int led_status = LOW;
 bool led_status_changed = false;
-void buttonPress()
+void buttonPress1()
 {
-  mortor_enable = 0;
-  Serial.print("모터 활성화=");
-  Serial.println(mortor_enable);
+  mortor_enable1 = 0;
+  Serial.print("모터 정지1=");
+  Serial.println(mortor_enable1);
 
   angle = 0; // 계산된 펄스값, 즉시 초기화 => 멈춤
   digitalWrite(13, LOW);
 
+  mortor_enable2 = 1;
+
 }
 
+void buttonPress2()
+{
+  mortor_enable2 = 0;
+  Serial.print("모터 정지2=");
+  Serial.println(mortor_enable2);
+
+  angle = 0; // 계산된 펄스값, 즉시 초기화 => 멈춤
+  digitalWrite(13, LOW);
+
+  mortor_enable1 = 1;
+
+}
 
 
 void loop() {  
@@ -89,7 +109,7 @@ void motor_cw(int deg)
 {
   int temp;
 
-  if(mortor_enable == 1) {
+  if(mortor_enable1 == 1) {
     // 입력한 각도에 따른 펄스수
     if(deg == 0) {
       angle = step;
@@ -107,7 +127,7 @@ void motor_cw(int deg)
   {
     Serial.println(angle);
 
-    if(mortor_enable == 1) {
+    if(mortor_enable1 == 1) {
       temp = i % 4;
       digitalWrite(pinArray[0], bitRead(CW[temp], 0));
       digitalWrite(pinArray[1], bitRead(CW[temp], 1));
@@ -125,7 +145,7 @@ void motor_ccw(int deg)
   int temp;
   
 
-  if(mortor_enable == 1) {
+  if(mortor_enable2 == 1) {
     // 입력한 각도에 따른 펄스수
     if(deg == 0) {
       angle = step;
@@ -143,7 +163,7 @@ void motor_ccw(int deg)
   {
     Serial.println(angle);
     
-    if(mortor_enable == 1) {
+    if(mortor_enable2 == 1) {
         temp = i % 4;
         digitalWrite(pinArray[0], bitRead(CCW[temp], 0));
         digitalWrite(pinArray[1], bitRead(CCW[temp], 1));
